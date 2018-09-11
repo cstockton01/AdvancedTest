67dec515238274b30e035b727a80d92fb9ec3bd5
Stockton <Stockton@172.17.161.203>
2018-09-11 12:27:37 -0500

    add a test message

--- AdvancedProgWork/src/chrisPackage/RookProgram.java
+++ AdvancedProgWork/src/chrisPackage/RookProgram.java
@@ -5,33 +5,52 @@ public class RookProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Test Comment! I AM HERE
+		
 		int[][]  RookArray  =  {  
-				{ 0, 1, 0, 0},
-                { 0, 0, 1, 0},
+				{ 0, 0, 1, 0},
+                { 0, 1, 0, 0},
                 { 1, 0, 0, 0},
                 { 0, 0, 0, 1}
              };
 	
-		rooksAreSafe(RookArray);
-	}
-
-public static void rooksAreSafe(int[][] a) {
-	
-	int rowTrack = 0;
-	for (int i = 0; i < a.length; i++){
-		for ( int j = 0; j < a.length; j++ ){
-			if (a[i][j] > 0) {
-				rowTrack++;
-				if (rowTrack > 1) {
-					System.out.println("False! Not Safe...");
-				} else {
-					System.out.println("Safe! The rook is okay.");
-				}
-				rowTrack--;
-			}
+		if (rooksAreSafe(RookArray) == true){
+			System.out.print("True");
+		} else {
+			System.out.print("False");
 		}
 		
 	}
+
+public static boolean rooksAreSafe(int[][] a) {
+	
+	int sum = 0;
+	
+	//Get the sum of each row and test it to see if another rook (another 1) is in the row.
+	for (int row = 0; row < a.length; row++) {
+		int sumRow = 0;
+		
+		for (int col = 0; col < a[row].length; col++) {
+			sumRow = sumRow + a[row][col];
+			//If the col has multiple 1's in it. Return fail...
+			if (sumRow > 1)	
+			return false;
+		}
+	}
+	
+	//Get the sum of each col and test it to see if another rook (another 1) is in the col.
+	for (int col = 0; col < a.length; col++) {
+		int colRow = 0;
+		
+		for (int row = 0; row < a[col].length; row++) {
+			colRow = colRow + a[row][col];
+			//If the col has multiple 1's in it. Return fail...
+			if (colRow > 1)	
+			return false;
+		}
+	}
+	
+	return true;
 }
 
 }
